d94dffd0c1079813336ab2d0431d7eafb2221f8c
Pjotr Savitski <gnum@tlu.ee>
2017-06-30 12:02:42 +0300

    Chnaged program logos.

--- views/default/ychange/front_page/sponsors.php
+++ views/default/ychange/front_page/sponsors.php
@@ -4,7 +4,11 @@
  */
 ?>
 <div class="elgg-grid clearfix">
-    <div class="elgg-col elgg-col-1of1 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/program.png'), 'alt' => 'logo']); ?>
+    <div class="elgg-col elgg-col-1of2 ychange-index-logo">
+        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/erasmus_plus.jpg'), 'alt' => 'logo']); ?>
+    </div>
+
+    <div class="elgg-col elgg-col-1of2 ychange-index-logo">
+        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/movetia.jpg'), 'alt' => 'logo']); ?>
     </div>
 </div>
