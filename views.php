9a6843870a2415fa3bb87a74e261d27b7d2d2734
Pjotr Savitski <gnum@tlu.ee>
2017-09-08 13:12:28 +0300

    Resolves #7, fixed a bug.

--- views.php
+++ views.php
@@ -7,6 +7,6 @@ return [
     'default' => [
         'logos/' => __DIR__ . '/graphics/logos',
         'backgrounds/' => __DIR__ . '/graphics/backgrounds',
-        'favicons' => __DIR__ . '/graphics/favicons',
+        'favicons/' => __DIR__ . '/graphics/favicons',
     ],
 ];
