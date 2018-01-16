ea289b2f6398f945046fe6087ad6ddd767554bae
Pjotr Savitski <gnum@tlu.ee>
2018-01-16 17:55:29 +0200

    Resolves #16, added all links.

--- views/default/ychange/front_page/partners.php
+++ views/default/ychange/front_page/partners.php
@@ -5,20 +5,48 @@
 ?>
 <div class="elgg-grid clearfix">
     <div class="elgg-col elgg-col-1of2 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/heidelberg.png'), 'alt' => 'logo']); ?>
+        <?php
+            echo elgg_view('output/url', [
+                'text' => elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/heidelberg.png'), 'alt' => 'logo']),
+                'href' => 'https://www.rgeo.de/en/start/',
+                'is_trusted' => true,
+                'target' => '_blank',
+            ]);
+        ?>
     </div>
 
     <div class="elgg-col elgg-col-1of2 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/cuni.png'), 'alt' => 'logo']); ?>
+        <?php
+            echo elgg_view('output/url', [
+                'text' => elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/cuni.png'), 'alt' => 'logo']),
+                'href' => 'https://www.natur.cuni.cz/geography/department-of-applied-geoinformatics-and-cartography',
+                'is_trusted' => true,
+                'target' => '_blank',
+            ]);
+        ?>
     </div>
 </div>
 
 <div class="elgg-grid clearfix">
     <div class="elgg-col elgg-col-1of2 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/fhnw.png'), 'alt' => 'logo']); ?>
+        <?php
+            echo elgg_view('output/url', [
+                'text' => elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/fhnw.png'), 'alt' => 'logo']),
+                'href' => 'http://www.gesellschaftswissenschaften-phfhnw.ch/ueber-uns/team/viehrig-kathrin-dr/',
+                'is_trusted' => true,
+                'target' => '_blank',
+            ]);
+        ?>
     </div>
 
     <div class="elgg-col elgg-col-1of2 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/tlu.png'), 'alt' => 'logo']); ?>
+        <?php
+            echo elgg_view('output/url', [
+                'text' => elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/tlu.png'), 'alt' => 'logo']),
+                'href' => 'http://htk.tlu.ee/htk/',
+                'is_trusted' => true,
+                'target' => '_blank',
+            ]);
+        ?>
     </div>
 </div>
--- views/default/ychange/front_page/sponsors.php
+++ views/default/ychange/front_page/sponsors.php
@@ -5,10 +5,24 @@
 ?>
 <div class="elgg-grid clearfix">
     <div class="elgg-col elgg-col-1of2 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/erasmus_plus.jpg'), 'alt' => 'logo']); ?>
+        <?php
+            echo elgg_view('output/url', [
+                'text' => elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/erasmus_plus.jpg'), 'alt' => 'logo']),
+                'href' => 'https://ec.europa.eu/programmes/erasmus-plus/node_en',
+                'is_trusted' => true,
+                'target' => '_blank',
+            ]);
+        ?>
     </div>
 
     <div class="elgg-col elgg-col-1of2 ychange-index-logo">
-        <?php echo elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/movetia.jpg'), 'alt' => 'logo']); ?>
+        <?php
+            echo elgg_view('output/url', [
+                'text' => elgg_view('output/img', ['src' => elgg_get_simplecache_url('logos/movetia.jpg'), 'alt' => 'logo']),
+                'href' => 'https://www.movetia.ch/',
+                'is_trusted' => true,
+                'target' => '_blank',
+            ]);
+        ?>
     </div>
 </div>
